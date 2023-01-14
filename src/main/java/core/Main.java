d6c936cd44095fcfc8705040e8f824786a2b5e4d
Vladyslav <Vladislavtkgum@gmail.com>
2023-01-14 15:34:07 +0200

    Solve small problems, change configuration

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -3,9 +3,13 @@ package core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@ComponentScan("config")
+@ComponentScan("api")
+@ComponentScan("dao")
+@ComponentScan("service")
+@PropertySource("classpath:application.yml")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
